9c0c595fe5c03652e8792bafd7ae73144f354830
MalshaniDahanyaka <mekaladahanayaka80@gmail.com>
2023-02-13 18:48:13 +0530

    Add ProfilePhoto components

---
+++ src/components/ProfilePhoto.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ProfilePhoto = ({ firstName, lastName }) => {
+  const initials = `${firstName[0]}${lastName[0]}`;
+  return (
+    <div style={{
+      width: '100px',
+      height: '100px',
+      borderRadius: '50%',
+      backgroundColor: '#f2f2f2',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '50px',
+      fontWeight: 'bold',
+      color: '#333',
+      marginLeft: '3.3vw',
+      marginTop: '3.3vw'
+    }}>
+      {initials}
+    </div>
+  );
+};
+
+export default ProfilePhoto;
